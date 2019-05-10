7175cdcc2e85584994d559f6c44f476e18b2bc17
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-05-10 11:18:11 -0400

    added more methods

--- Book.js
+++ Book.js
@@ -1,7 +1,12 @@
 class Book {
-  constructor(title, author, isbn) {
+  constructor(title, author, isbn, patron, dueDate, out) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
+    this.patron = null;
+    this.dueDate = null;
+    this.out = false;
   }
+
+  
 }
--- Patron.js
+++ Patron.js
@@ -4,6 +4,21 @@ class Patron {
     this.email = email;
     this.currentBook = null;
   }
-  
-  
+
+  checkOut(book){
+    this.currentBook = book;
+    book.out = true;
+    book.patron = this;
+
+    const newDueDate = new Date();
+        newDueDate.setDate(newDueDate.getDate() + 14);
+        book.dueDate = newDueDate;
+  }
+
+  returnBook(book){
+    this.currentBook = null;
+    book.out = false;
+    book.patron = null;
+    book.dueDate = null;
+  }
 }
