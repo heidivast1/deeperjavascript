f33d9645ec3189d42628cd9bb98f20d8054f235a
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-05-10 17:13:07 -0400

    addition of get/set

--- Book.js
+++ Book.js
@@ -1,12 +1,27 @@
 class Book {
-  constructor(title, author, isbn, patron, dueDate, out) {
+  constructor(title, author, isbn) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
     this.patron = null;
     this.dueDate = null;
-    this.out = false;
+    this._out = false;
   }
 
-  
+  set out(out){
+        this._out = out;
+
+        if (out) {
+            const newDueDate = new Date();
+            newDueDate.setDate(newDueDate.getDate() + 14);
+            this.dueDate = newDueDate;
+        } else {
+            this.dueDate = null;
+        }
+   }
+
+   get out() {
+        return this._out;
+   }
+
 }
