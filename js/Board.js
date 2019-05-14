c703cdb2224df174ac6956c176971189eb5a9ed0
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-05-14 14:20:11 -0400

    initiating methods to conect the classes

--- js/Board.js
+++ js/Board.js
@@ -1,3 +1,31 @@
 class Board {
-  
-}
+  constructor(/*no argumnets because each game will have the same amount of rows and columns, so we statically set*/) {
+    this.rows = 6;
+    this.columns = 7;
+    this.spaces = this.createSpaces();
+  }
+
+
+  /**
+ * Generates 2D array of spaces.
+ * @return  {Array}     An array of space objects
+ */
+
+
+ createSpaces(){
+     const spaces = [];
+
+     for (let x = 0; x < this.columns; x++){
+       const column = []; /*will hold the Space objects for each column
+       first column variable gets set to empty array, then we write another for loop to iterate over rows of t column*/
+
+       for (let y = 0; y < this.rows; y++){ /*new Space objects are created*/
+         const space = new Space(x, y); /*pass space constructor method values of the x and y index*/
+         column.push(space); /*push newly created space to the column array, on each iteration through loop another apce is added to the column*/
+      }
+
+      spaces.push(column); /*push entire column into Spcaes array, then the x index is increased by 1 and the process continues*/
+    }
+     return spaces;
+   }
+ }
--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,18 @@
 class Game {
-  
+  constructor() {
+    this.board = new Board();
+    this.players = this.createPlayers(); /*when new game object create, so are new players, then so are all the Token Objects! */
+    this.ready = false;
+  }
+
+  /**
+ * Creates two player objects
+ * @return  {Array}    An array of two Player objects.
+ */
+
+  createPlayers() {
+    const players = [new Player('Player 1', 1, '#e15258', true),
+                    new Player('Player 2', 2, '#e59a13')];
+    return players;
+  }
 }
