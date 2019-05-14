7829b288936b8f32a273c3ab2674922a9e48eb73
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-05-14 12:07:06 -0400

    creation of tokens via methods

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,25 @@ class Player {
     this.id = id;
     this.color = color;
     this.active = active;
-    this.tokens = [];
+    this.tokens = this.createTokens(21);
+  }
+
+  /**
+  * Creates token objects for player
+  * @param   {integer}   num - Number of token objects to be created
+  * @return  {array}     tokens - an array of new token objects
+  */
+  createTokens(num){
+    const tokens = [];
+
+    for (let i = 0; i < num; i++){
+      let token = new Token(i, this);
+      tokens.push(token);
+      /*add newly crfeated token to token array by using the push method
+        when loop is finished and all the tokens have been created return the array
+        where is it being returned to thou? see constructor
+      */
+    }
+    return tokens;
   }
 }
